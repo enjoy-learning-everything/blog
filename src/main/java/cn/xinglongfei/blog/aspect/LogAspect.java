151f51cc90aa2372d347fc7dd4e7adf6403624f4
TheYoungLad <1828919295@qq.com>
2021-01-10 22:18:22 +0800

    日志记录过滤爬虫、上线博客演示Demo

--- src/main/java/cn/xinglongfei/blog/aspect/LogAspect.java
+++ src/main/java/cn/xinglongfei/blog/aspect/LogAspect.java
@@ -5,8 +5,6 @@ import cn.xinglongfei.blog.po.Log;
 import cn.xinglongfei.blog.po.User;
 import cn.xinglongfei.blog.service.LogService;
 import cn.xinglongfei.blog.util.RequestUtil;
-import eu.bitwalker.useragentutils.Browser;
-import eu.bitwalker.useragentutils.OperatingSystem;
 import eu.bitwalker.useragentutils.UserAgent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -41,6 +39,7 @@ public class LogAspect {
     private LogService sysLogService;
 
 
+
     /**
      * 我这里是使用log4j2把一些信息打印在控制台上面，可以不写
      */
@@ -60,68 +59,72 @@ public class LogAspect {
         //用于保存日志
         Log sysLog = new Log();
 
-        //从切面织入点处通过反射机制获取织入点处的方法
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        //获取切入点所在的方法
-        Method method = signature.getMethod();
-
-        //获取操作--方法上的Log的值
-        MyLog myLog = method.getAnnotation(MyLog.class);
-        if (myLog != null) {
-            //保存操作事件
-            String operation = myLog.operation();
-            sysLog.setOperation(operation);
-
-            //保存日志类型
-            String type = myLog.type();
-            sysLog.setType(type);
-
-            log.info("operation=" + operation + ",type=" + type);
-        }
-
-        //获取请求的类名
-        String className = joinPoint.getTarget().getClass().getName();
-        //获取请求的方法名
-        String methodName = method.getName();
-        sysLog.setMethod(className + "." + methodName);
-
-        //请求的参数
-        Object[] argsTemp = joinPoint.getArgs();
-////        System.out.println(argsTemp);
-////        //将参数所在的数组转换成json
-//        String args = JSON.toJSONString(argsTemp, SerializerFeature.WriteNullStringAsEmpty);
-        sysLog.setArgs(Arrays.toString(argsTemp));
-
         // 客户端ip
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
         String ip = RequestUtil.getIpAddr(request);
         sysLog.setIp(ip);
-        log.info("methodName=" + methodName, "ip=" + ip);
 
         //获取用户的浏览器版本以及操作系统版本
         UserAgent userAgent = UserAgent.parseUserAgentString(request.getHeader("User-Agent"));
-        Browser browser = userAgent.getBrowser();
-        OperatingSystem os = userAgent.getOperatingSystem();
-        sysLog.setBrowser(browser.toString());
-        sysLog.setSystem(os.toString());
-        log.info("Browser=" + browser.toString(), "OperatingSystem=" + os.toString());
+        String browser = userAgent.getBrowser().getName();
+        String os = userAgent.getOperatingSystem().getName();
+        String osTemp = os.toUpperCase();
+        sysLog.setBrowser(browser);
+        sysLog.setSystem(os);
+        log.info("Browser=" + browser, "OperatingSystem=" + os);
 
-        // 操作人账号、姓名（需要提前将用户信息存到session）
-        User user = (User) request.getSession().getAttribute("user");
-        if (user != null) {
-            Long userId = user.getId();
-            String userName = user.getUsername();
-            sysLog.setUserId(userId);
-            sysLog.setUsername(userName);
-            log.info("userId=" + userId, "userName=" + userName);
-        }
-        //设置时间
-        sysLog.setOperaTime(new Date());
+        //只记录真实用户的IP，过滤爬虫（Robot）的日志记录，爬虫的系统为Unkown，虽然工具类有部分系统不能识别，但是不能识别的部分很少很少
+        if (!osTemp.equals("UNKNOWN")) {
 
-        //调用service保存SysLog实体类到数据库，且只记录非调试者IP
-        if(!sysLog.getIp().equals("0:0:0:0:0:0:0:1")&&!sysLog.getIp().equals("127.0.0.1")){
+            //从切面织入点处通过反射机制获取织入点处的方法
+            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+            //获取切入点所在的方法
+            Method method = signature.getMethod();
+
+
+            //获取操作--方法上的Log的值
+            MyLog myLog = method.getAnnotation(MyLog.class);
+            if (myLog != null) {
+                //保存操作事件
+                String operation = myLog.operation();
+                sysLog.setOperation(operation);
+
+                //保存日志类型
+                String type = myLog.type();
+                sysLog.setType(type);
+
+                log.info("operation=" + operation + ",type=" + type);
+            }
+
+            //获取请求的类名
+            String className = joinPoint.getTarget().getClass().getName();
+            //获取请求的方法名
+            String methodName = method.getName();
+            sysLog.setMethod(className + "." + methodName);
+            log.info("methodName=" + methodName, "ip=" + ip);
+
+            //请求的参数
+            Object[] argsTemp = joinPoint.getArgs();
+            sysLog.setArgs(Arrays.toString(argsTemp));
+
+
+            // 操作人账号、姓名（需要提前将用户信息存到session）
+            User user = (User) request.getSession().getAttribute("user");
+            if (user != null) {
+                Long userId = user.getId();
+                String userName = user.getUsername();
+                sysLog.setUserId(userId);
+                sysLog.setUsername(userName);
+                log.info("userId=" + userId, "userName=" + userName);
+            }
+            //设置时间
+            sysLog.setOperaTime(new Date());
+
+            //调用service保存SysLog实体类到数据库
             sysLogService.saveLog(sysLog);
+
         }
+
 
     }
 
